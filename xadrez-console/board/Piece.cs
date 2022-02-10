13213daec6c18f7e7e1a60289ba669f769de634d
Savio Caetano <saviocardoso27@gmail.com>
2022-02-10 16:32:22 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/board/Piece.cs
+++ xadrez-console/board/Piece.cs
@@ -7,9 +7,9 @@
         public int numberOfMoves { get; protected set; }
         public Board board { get; protected set; }
 
-        public Piece(Position position, Color color, Board board)
+        public Piece(Board board, Color color)
         {
-            this.position = position;
+            this.position = null;
             this.color = color;
             this.board = board;
             this.numberOfMoves = 0;
---
+++ xadrez-console/chess/Peon.cs
@@ -0,0 +1,16 @@
+﻿using board;
+
+namespace chess
+{
+    class Peon : Piece
+    {
+        public Peon(Board board, Color color) : base(board, color)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "P";
+        }
+    }
+}
